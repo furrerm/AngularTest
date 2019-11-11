93bd82ab69d95eeef68a4510740c713d98e9dcfe
tnfurrer <markus.furrer@gmail.com>
2019-11-11 05:39:58 +0100

    initializing commit (fully working hero page with heroes as mocks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AngularTest';
+  title = 'Tour of Heroes Tutorial';
+  message = 'Hello World test';
 }
---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+//import { HeroDetailComponent } '../hero.detail/hero-detail'
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+
+export class HeroesComponent implements OnInit {
+
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit() {
+	  this.getHeroes();
+  }
+ 
+	
+	getHeroes(): void {
+  this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+}
+  
+  heroes: Hero[];
+  
+}
